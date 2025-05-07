906813a294cef5387516c1fb94ad4e1365cf689e
Lukas Duzakin <125671838+Podolskigutten@users.noreply.github.com>
2025-05-07 14:53:08 +0200

    Added main funtion as well as first steps into loading images

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import matplotlib as plt
 import cv2 as cv
 from dataloader import ImageLoader
 
-# Load data from working directory
-path = os.path.join("Images", "00", "image_0")
 
-loader = ImageLoader(path, 1)
-images = loader.load_images()
 
-print(f"Loaded {len(images)} images")
+def main():
+
+    #start by loading images
+    path = os.path.join("Images", "00", "image_0")
+
+    loader = ImageLoader(path, 2)
+    images = loader.load_images()
+    print(f"Loaded {len(images)} images")
+
+
+
+if __name__ == "__main__":
+    main()
